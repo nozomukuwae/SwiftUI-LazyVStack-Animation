3c8ca9625e0e0d570c499a83cc8723b467b1e60a
Nozomu Kuwae <nozomukuwae@gmail.com>
2025-01-27 18:33:40 +0900

    Initial Commit

---
+++ LazyVStackAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LazyVStackAnimation
+//
+//  Created by Nozomu Kuwae on 2025/01/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LazyVStackAnimation/LazyVStackAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVStackAnimationApp.swift
+//  LazyVStackAnimation
+//
+//  Created by Nozomu Kuwae on 2025/01/27.
+//
+
+import SwiftUI
+
+@main
+struct LazyVStackAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
