03b0a969a8a6ebd30bce9a17191f53d8553a6fd2
Nozomu Kuwae <nozomukuwae@gmail.com>
2025-01-29 14:05:36 +0900

    Explicit animation assuming items are given from outside

--- LazyVStackAnimation/ContentView.swift
+++ LazyVStackAnimation/ContentView.swift
@@ -12,33 +12,70 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            LazyVStack {
-                Button("Append") {
-                    let lastValue = numbers.last ?? -1
-                    numbers.append(lastValue+1)
-                }
-                .padding()
-                
-                ForEach(numbers, id: \.self) { number in
-                    Text(String(number))
-                        .padding()
-                        .background(
-                            Color.mint,
-                            ignoresSafeAreaEdges: .horizontal
-                        )
-                        .transition(.opacity)
-                        .onTapGesture {
-                            if let index = numbers.firstIndex(where: { $0 == number }) {
-                                _ = withAnimation(.easeIn(duration: 1.0)) {
-                                    numbers.remove(at: index)
-                                }
-                            }
-                        }
-                }
+            Button("Append") {
+                let lastValue = numbers.last ?? -1
+                numbers.append(lastValue+1)
             }
+            .padding()
+
+            ListView(
+                numbers: $numbers,
+                onTapNumber: { number in
+                    if let index = numbers.firstIndex(where: { $0 == number }) {
+                        numbers.remove(at: index)
+                    }
+                }
+            )
 
             Spacer()
         }
+    }
+}
+
+struct ListView: View {
+    @Binding var newNumbers: [Int]
+    @State private var numbers: [Int]
+    let onTapNumber: (Int) -> Void
+    
+    init(numbers: Binding<[Int]>, onTapNumber: @escaping (Int) -> Void) {
+        self._newNumbers = numbers
+        self.numbers = numbers.wrappedValue
+        self.onTapNumber = onTapNumber
+    }
+    
+    var body: some View {
+        LazyVStack {
+            ForEach(numbers, id: \.self) { number in
+                Text(String(number))
+                    .padding()
+                    .background(
+                        Color.mint,
+                        ignoresSafeAreaEdges: .horizontal
+                    )
+                    .transition(.opacity)
+                    .onTapGesture { onTapNumber(number) }
+            }
+        }
+        .onChange(of: newNumbers, initial: false) {
+            if newNumbers.count < numbers.count {
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    // Works when only one item is removed at once
+                    if let index = numbers.indices.first(where: {
+                        numbers[$0] != newNumbers[safe: $0]
+                    }) {
+                        numbers.remove(at: index)
+                    }
+                }
+            } else {
+                numbers = newNumbers
+            }
+        }
+    }
+}
+
+extension Collection {
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
     }
 }
 
